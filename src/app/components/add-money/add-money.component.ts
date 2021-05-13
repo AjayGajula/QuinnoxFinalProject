7eb4c46dbca3b94dafd60a6bb0f85586573c6d26
Ritesh Kumar <kumar.ritesh97@hotmail.com>
2021-05-14 00:45:56 +0530

    update

--- src/app/components/add-money/add-money.component.ts
+++ src/app/components/add-money/add-money.component.ts
@@ -1,13 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Router } from '@angular/router';
-import { CommonServiceService } from 'src/app/services/common-service.service';
-
-
-import { NgForm } from '@angular/forms';
-import { FormBuilder, Validators, ControlContainer, FormGroup, FormControl } from '@angular/forms';
-=======
->>>>>>> 5fcac76c3ce0402feb66129c81d4a068a5599f3c
 
 @Component({
   selector: 'app-add-money',
@@ -15,7 +6,6 @@ import { FormBuilder, Validators, ControlContainer, FormGroup, FormControl } fro
   styleUrls: ['./add-money.component.css']
 })
 export class AddMoneyComponent implements OnInit {
-<<<<<<< HEAD
   title: string;
   amount: number;
   cardHolder: string;
@@ -36,12 +26,5 @@ export class AddMoneyComponent implements OnInit {
   addMoney() {
     alert(this.amount)
   }
-=======
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
->>>>>>> 5fcac76c3ce0402feb66129c81d4a068a5599f3c
 
 }
